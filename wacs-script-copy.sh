690dd2a18ea03d46097d112631a2ea1023b0eaab
Craig Oliver <craig.robert.oliver@gmail.com>
2020-09-04 10:35:10 -0400

    WIP check file

--- wacs-script-copy.sh
+++ wacs-script-copy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Make sure file was given
+if [ -z "${1}" ]; then
+    echo "Usage: ${0} input_filename"
+    echo "Reads input file containing repo URLs and copies them to WACS/Staging"
+    exit 1
+fi
+
+# Make sure environment variables are set
 REQUIRED_ENV_VARS=(GITEA_API_TOKEN GITEA_URL GITEA_USER)
 for env_var in ${REQUIRED_ENV_VARS[@]}; do
     if [ -z "${!env_var}" ]; then
         echo "ERROR: Missing require env var: ${env_var}"
+        exit 1
     fi
 done
+
